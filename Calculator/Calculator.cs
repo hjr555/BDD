fcbca9142f30833dad325ed332778d1f15bf5839
Hywel Rees <hjr555@gmail.com>
2020-05-25 17:55:15 +0100

    Tests fail correctly

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Calculator
+{
+    public object Result { get; set; }
+
+    public void SetFirstNumber(int firstNumber)
+    {
+    }
+
+    public void SetSecondNumber(int secondNumber)
+    {
+    }
+
+    public void AddNumbers()
+    {
+    }
+}
